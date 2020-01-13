1364e40aac801eaf6de0bf6c24c0adcadc2051c3
Josi Reis-West <josialexa85@gmail.com>
2020-01-13 12:11:34 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import menuButton from './menu-alt-512.png'
+import './reset.css'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <div className='responsive-header'>
+          <span className='header-text'>Start Bootstrap</span>
+          <button className='button responsive-button'>MENU <i class="fas fa-bars"></i></button>
+          <ul className='responsive-menu'>
+            <li className='menu-item'>SERVICES</li>
+            <li className='menu-item'>PORTFOLIO</li>
+            <li className='menu-item'>ABOUT</li>
+            <li className='menu-item'>TEAM</li>
+            <li className='menu-item'>CONTACT</li>
+          </ul>
+        </div>
+        <div className='responsive-banner'>
+          <span className='welcome'>Welcome To Our Studio!</span>
+          <h1 className='h1'>IT'S NICE TO MEET YOU</h1>
+          <button className='button'>TELL ME MORE</button>
+        </div>
+      </div>
     </div>
   );
 }
